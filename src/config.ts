5a43e49103593def80cb687e9ab0dbb5a13b5f55
Stanley Chibuike <chibuike.stanley50@gmail.com>
2024-10-25 10:33:22 +0100

    Added new features

--- src/config.ts
+++ src/config.ts
@@ -1,2 +1,3 @@
 export const BASE_URL = "https://tutkfvux6p.eu-west-1.awsapprunner.com/api";
 export const INVOICE_URL = "/v1/invoices";
+export const COLLECTION_URL = "/v1/collections";
--- src/tranzakt.ts
+++ src/tranzakt.ts
@@ -1,25 +1,78 @@
-import { INVOICE_URL } from "./config";
-import { requestProcessor } from "./request-processor";
-import { CreateInvoiceProps, Invoice } from "./types";
+import { COLLECTION_URL, INVOICE_URL } from "./config";
+import { BaseResponse, requestProcessor } from "./request-processor";
+import {
+  Collection,
+  CreateInvoiceProps,
+  GetCollectionInvoicesParams,
+  GetCollectionInvoicesResponse,
+  Invoice,
+} from "./types";
 
 export class Tranzakt {
   constructor(private readonly secretKey: string) {}
 
-  async getInvoiceDetails(invoiceId: string) {
+  async getInvoiceDetails(invoiceId: string): Promise<Invoice> {
     return await requestProcessor<Invoice>({
       url: `${INVOICE_URL}/${invoiceId}`,
       method: "GET",
       headers: { "x-api-key": this.secretKey },
+      expectsData: true,
     });
   }
 
-  async createInvoice(dynamicInvoice: CreateInvoiceProps) {
-    const response = await requestProcessor<Invoice>({
+  async createInvoice(dynamicInvoice: CreateInvoiceProps): Promise<Invoice> {
+    return await requestProcessor<Invoice>({
       data: dynamicInvoice,
       url: INVOICE_URL,
       method: "POST",
       headers: { "x-api-key": this.secretKey },
+      expectsData: true,
     });
-    return response;
+  }
+
+  async invalidateAnInvoice(invoiceId: string): Promise<BaseResponse> {
+    return await requestProcessor<BaseResponse>({
+      url: `${INVOICE_URL}/${invoiceId}/invalidate`,
+      method: "POST",
+      headers: { Authorization: `Bearer ${this.secretKey}` },
+      expectsData: false,
+    });
+  }
+
+  async getCollectionDetails(collectionId: string): Promise<Collection> {
+    return await requestProcessor<Collection>({
+      url: `${COLLECTION_URL}/${collectionId}`,
+      method: "GET",
+      headers: { Authorization: `Bearer ${this.secretKey}` },
+      expectsData: true,
+    });
+  }
+
+  async getCollectionInvoices(
+    collectionId: string,
+    params?: GetCollectionInvoicesParams
+  ): Promise<GetCollectionInvoicesResponse> {
+    // Convert params object to URLSearchParams
+    const queryParams = new URLSearchParams();
+
+    if (params) {
+      Object.entries(params).forEach(([key, value]) => {
+        if (value !== undefined) {
+          queryParams.append(key, value.toString());
+        }
+      });
+    }
+
+    // Construct URL with query parameters
+    const url = `${COLLECTION_URL}/${collectionId}/Invoices${
+      queryParams.toString() ? `?${queryParams.toString()}` : ""
+    }`;
+
+    return await requestProcessor<GetCollectionInvoicesResponse>({
+      url,
+      method: "GET",
+      headers: { Authorization: `Bearer ${this.secretKey}` },
+      expectsData: true,
+    });
   }
 }
